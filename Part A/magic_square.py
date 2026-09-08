e8a13e136265eb5f91dd3c28a3ebb9eeb5c901fb
paula456 <2466776@cegep-heritage.qc.ca>
2026-09-08 11:20:11 -0400

    Add basic validation for number row input

--- Part A/magic_square.py
+++ Part A/magic_square.py
@@ -10,5 +10,14 @@ for x in range(2, number_of_rows + 1):
     row = input(f"Enter row {x} of the magic square: ")
     rows.append([int(x) for x in row.split()])
 
-for i in rows:
-    print(i)
+range_list = sorted([number for sublist in rows for number in sublist])
+
+if len(range_list) > number_of_rows ** 2  or len(range_list) < number_of_rows ** 2:
+     print(f"The square must have {number_of_rows ** 2} numbers.")
+
+if range_list[-1] > number_of_rows ** 2 or range_list[0] < 1:
+    print("The square does not respect the range constraint.")
+
+
+if len(range_list) != len(set(range_list)):
+    print("The square cannot have duplicates.")
