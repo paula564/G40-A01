61b69d16c5a442b989780fb80d5026f370ed75d7
paula456 <2466776@cegep-heritage.qc.ca>
2026-09-15 10:43:02 -0400

    Commit current progress

--- Part A/magic_square.py
+++ Part A/magic_square.py
@@ -1,3 +1,6 @@
+#put everything under main 
+#need to do testing for this
+
 import sys
 
 def is_sum_magic(value, magic_number):
@@ -21,7 +24,7 @@ def is_valid_diagonals(diagonals):
             return False
     return True
 
-
+no_invalid_input = True
 rows = []
 first_row = input("Enter row 1 of the magic square: ")
 rows.append([int(x) for x in first_row.split()])
@@ -33,24 +36,29 @@ MAGIC_NUMBER = (number_of_rows * (number_of_rows ** 2 + 1)) // 2
 
 for x in range(2, number_of_rows + 1):
     row = input(f"Enter row {x} of the magic square: ")
-    if len(row.split()) != number_of_rows:
+    rows.append([int(x) for x in row.split()])
+
+for row in rows:
+    if len(row) != number_of_rows:
         print(f"Each row must have {number_of_rows} numbers.")
-        sys.exit(0)
-    else:
-        rows.append([int(x) for x in row.split()])
+        no_invalid_input = False
+        break
 
 range_list = sorted([int(number) for sublist in rows for number in sublist])
 
 if len(range_list) > number_of_rows ** 2  or len(range_list) < number_of_rows ** 2:
     print(f"The square must have {number_of_rows ** 2} numbers.")
-    sys.exit(0)
+    no_invalid_input = False
 
 if any(number not in range(1, (number_of_rows ** 2) + 1 ) for number in range_list):
     print("The square does not respect the range constraint.")
-    sys.exit(0)
+    no_invalid_input = False
 
 if len(range_list) != len(set(range_list)):
     print("The square cannot have duplicates.")
+    no_invalid_input = False
+
+if not no_invalid_input:
     sys.exit(0)
 
 #passes three separate lists into zip instead of one big list. matches the items based on index (index 0s with index 0s, etc.)
@@ -66,10 +74,10 @@ diagonals.append(anti_diagonal)
 
 if not is_valid_rows(rows) or not is_valid_columns(columns) or not is_valid_diagonals(diagonals):
     print(f"This is not a magic square. The sum of each row, column and diagonal is not {MAGIC_NUMBER}.")
-    sys.exit(0)
+    
 else:
     print(f"This is a magic square! The sum of each row, column and diagonal is {MAGIC_NUMBER}.")
-    sys.exit(0)
+   
 
 
 
---
+++ Part B/todo_list.py
@@ -0,0 +1,14 @@
+class Task:
+    def __init__(self, task_id: int, desc: str, completed: bool, project: str = None):
+        self.task_id = task_id      
+        self.desc = desc
+        self.completed = completed  
+        self.project = project
+      
+
+def main():
+   print()
+    
+
+if __name__ == "__main__":
+    main()
