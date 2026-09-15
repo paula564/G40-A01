dab16d494f415888e4399136859a2ce77d6fd59a
paula456 <2466776@cegep-heritage.qc.ca>
2026-09-15 11:12:46 -0400

    Implement and test adding task functionality

--- Part B/todo_list.py
+++ Part B/todo_list.py
@@ -4,10 +4,29 @@ class Task:
         self.desc = desc
         self.completed = completed  
         self.project = project
-      
+
+def add_task(tasks, string):
+    if "#" in string:
+        description = string.split(" ", 1)[1]
+        task_description, task_project = description.split("#", 1)[0].rstrip(), description.split("#", 1)[1].rstrip()
+    else:
+        task_description, task_project = string.split(" ", 1)[1], None
+
+    task_id = len(tasks) + 1 if len(tasks) > 0 else 1
+    task_object = Task(task_id, task_description, False, task_project)
+    tasks[task_id] = task_object.__dict__
+    
 
 def main():
-   print()
+   tasks = {}
+   while True:
+        command = input("Please enter a command: ")
+        if command.split(" ", 1)[0] == "add":
+            add_task(tasks, command)
+            print(f"The task object: {tasks[1]}")
+            print(f"The list of tasks: {tasks}")
+        if command.split(" ", 1)[0] == "exit":
+            break
     
 
 if __name__ == "__main__":
